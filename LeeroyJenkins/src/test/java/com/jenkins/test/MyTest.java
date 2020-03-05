dd4ac89760066262e717a7b5c0b3074abbc1fe3b
Jordan Zech <s521476@nwmissouri.edu>
2020-03-05 14:58:28 -0500

    spaces

--- LeeroyJenkins/src/test/java/com/jenkins/test/MyTest.java
+++ LeeroyJenkins/src/test/java/com/jenkins/test/MyTest.java
@@ -1,5 +1,14 @@
 package com.jenkins.test;
 
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+
 public class MyTest {
 
+	@Test
+	public void test() {
+		assertEquals("True is true", true, true);
+	}
 }
