77fab437d445eff03b80c820c1d94c946e599f91
Jordan Zech <s521476@nwmissouri.edu>
2020-03-05 14:40:55 -0500

    changes

--- LeeroyJenkins/src/main/java/com/jenkins/servlet/HelloServlet.java
+++ LeeroyJenkins/src/main/java/com/jenkins/servlet/HelloServlet.java
@@ -7,11 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 public class HelloServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().write("I am a yellowsubmarine");
+		resp.getWriter().write("I am any yellowsubmarine");
 	}
 	
 }
